ac4f4e2ad317c7e6c6af2c8e5672879e08cc3924
Mutsuki <yukkeca@outlook.jp>
2023-11-23 22:45:48 +0900

    未使用部分を削除

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -1,28 +1,27 @@
-import React from 'react';
-import logo from '@assets/img/logo.svg';
 import '@pages/newtab/Newtab.css';
 import '@pages/newtab/Newtab.scss';
-import useStorage from '@src/shared/hooks/useStorage';
-import exampleThemeStorage from '@src/shared/storages/exampleThemeStorage';
 import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import BookmarkCard from '@root/src/components/bookmarkCard/bookmarkCard';
+import TreeView from '@root/src/components/treeView/treeView';
 
 const Newtab = () => {
-  const theme = useStorage(exampleThemeStorage);
-
+ 
   return (
     <div
       className="App">
-        <div className='container'>
+        <div className='container grid'>
           <BookmarkCard title="Youtube" url="https://www.youtube.com/" />
           <BookmarkCard title="はてなブログ" />
           <BookmarkCard title="X" />
           <BookmarkCard title="Wantedly" />
           <BookmarkCard title="GitHub" />
           <BookmarkCard title="Amazon" />
-          <BookmarkCard title="Pixiv" />
+          <BookmarkCard title="Pixiv" />          
 
+        </div>
+        <div className='container'>          
+            <TreeView />          
         </div>
     </div>
   );
