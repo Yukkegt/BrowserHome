6fde5f7b2b17acea8d3eddece05b4b594fff3ec0
Mutsuki <yukkeca@outlook.jp>
2023-11-25 21:54:54 +0900

    新規追加ボタンを追加(ガワのみ)

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -4,11 +4,18 @@ import withSuspense from '@src/shared/hoc/withSuspense';
 import withErrorBoundary from '@src/shared/hoc/withErrorBoundary';
 import BookmarkCard from '@root/src/components/bookmarkCard/bookmarkCard';
 import TreeView from '@root/src/components/treeView/treeView';
+import { FaPlus } from "react-icons/fa6";
 
 const Newtab = () => {
  
   return (
     <div className="App">
+      <div className='container'>
+      <button className='add-button' >
+        <FaPlus/>
+      </button>
+
+      </div>
         <div className='container grid'>
           <BookmarkCard title="Youtube" url="https://www.youtube.com/" />
           <BookmarkCard title="はてなブログ" />
