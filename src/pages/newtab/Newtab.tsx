34f16b46fc3e0ee5ad52d386fe45c577e0a84d34
Mutsuki <yukkeca@outlook.jp>
2023-12-03 13:54:45 +0900

    ブックマーク追加時のsubmit処理

--- src/pages/newtab/Newtab.tsx
+++ src/pages/newtab/Newtab.tsx
@@ -11,36 +11,44 @@ import bookmarkData from '../../shared/jsonStorages/bookmark';
 const Newtab = () => {
   const [bookmarks, setBookmarks] = useState(bookmarkData);  
   const [isAddMode, setAddMode] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
+  const [newURL, setURL] = useState("");
+  const onInputTitleChanged = (e) => setNewTitle(e.target.value);
+  const onInputURLChanged = (e) => setURL(e.target.value);
+
+  function submitBookmark(event) {
+    event.preventDefault();
+    console.log(newTitle);
+    console.log(newURL);
+  }
 
   return (
     <div className="App">
       <div className='container'>
-      <form className='add-button-area'>
-        <button type='button' title='ブックマークを追加' className={`add-button ${isAddMode ? 'active' : ''}`} onClick={() => setAddMode(!isAddMode)} >
-          <FaPlus className='add-bookmark-icon'/>
-        </button>
-        {isAddMode ? 
-          <>
-            <input className='add-bookmark-input fade-down' title='タイトル' placeholder='タイトル'></input>
-            <input className='add-bookmark-input fade-down' title='URL' placeholder='URL'></input>
-            <button type='submit' className='submit-allow fade-down' title='ブックマークを登録'>
-              <FaArrowRightLong />
-            </button>
-          </>
-          :<></>          
-        }
-      </form>
-      
-
+        <form className='add-button-area' onSubmit={submitBookmark}>
+          <button type='button' title='ブックマークを追加' className={`add-button ${isAddMode ? 'active' : ''}`} onClick={() => setAddMode(!isAddMode)} >
+            <FaPlus className='add-bookmark-icon'/>
+          </button>
+          {isAddMode ? 
+            <>
+              <input className='add-bookmark-input fade-down' title='タイトル' placeholder='タイトル' value={newTitle} onChange={onInputTitleChanged}></input>
+              <input className='add-bookmark-input fade-down' title='URL' placeholder='URL' value={newURL} onChange={onInputURLChanged}></input>
+              <button type='submit' className='submit-allow fade-down' title='ブックマークを登録'>
+                <FaArrowRightLong />
+              </button>
+            </>
+            :<></>          
+          }
+        </form>
       </div>
-        <div className='container grid'>
-          {bookmarks.bookmarks.map((bookmark) => (
-            <BookmarkCard propTitle={bookmark.title} propUrl={bookmark.url} />            
-          ))}
-        </div>
-        <div className='container'>          
-            <TreeView />          
-        </div>
+      <div className='container grid'>
+        {bookmarks.bookmarks.map((bookmark) => (
+          <BookmarkCard propTitle={bookmark.title} propUrl={bookmark.url} />            
+        ))}
+      </div>
+      <div className='container'>          
+          <TreeView />          
+      </div>
     </div>
   );
 };
